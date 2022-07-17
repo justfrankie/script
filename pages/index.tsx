957eb0a5c69b82aca3bc6b7871b745533000370e
justfrankie <zhuangfrankie@gmail.com>
2022-07-17 02:37:55 -0700

    remove unused variables

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,4 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import { ReactNode } from 'react';
-import { render } from 'react-dom';
 import styles from '../styles/Dashboard.module.css'
 import {mockEntries} from "./api/constants/mockEntries";
 const Dashboard: NextPage = () => {
